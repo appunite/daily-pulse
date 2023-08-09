10533d2a22b26086cfc6b75cf6101bd69729156e
Adrian Orłów <adrian.orlow@fishbrain.com>
2023-08-09 17:44:00 +0200

    feat: Remove bloat code

--- daily_pulse/utils/page.py
+++ daily_pulse/utils/page.py
@@ -39,11 +39,3 @@ def getPageWithDocument(pageObject):
         "url": standarized_page["url"],
         "document": page_document,
     }
-
-    return {
-        "id": "06151dd6-4767-46eb-8282-7fdd02ffb328",
-        "url": "https://www.notion.so/Jerry-joins-OPNR-FT-for-two-weeks-06151dd6476746eb82827fdd02ffb328",
-        "document": Document(
-            page_content="Title: Jerry joins OPNR FT for two weeks Type: Decision Status: Decided URL: https://www.notion.so/Jerry-joins-OPNR-FT-for-two-weeks-06151dd6476746eb82827fdd02ffb328 --- # Context Context is written in below document, this ADR is created for hygiene reasons. [https://www.notion.so/appunite/Increase-Jerry-engagement-in-OPNR-for-FT-600330c2a91f45ad892ad6e38653adca](https://www.notion.so/appunite/Increase-Jerry-engagement-in-OPNR-for-FT-600330c2a91f45ad892ad6e38653adca) # What problems do we aim to solve There is a lot of product design work right now which can be a bottleneck. # Hypothesis Having Jerry FT for 2 weeks boosts product and won’t affect client’s finances. # Implementation plan Jerry is working FT in OPNR from 07.08.2023 - 18.08.2023 # Changelog * / - * *List important moments for this card* Damian Dębicki / Aug 9, 2023 - card created"
-        ),
-    }
